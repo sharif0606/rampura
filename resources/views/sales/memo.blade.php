b8b25ce5b15da10ab155ef94647b3cc00364eaec
BIPLAB UDDIN <biplabuddin990@gmail.com>
2023-09-19 17:51:26 +0600

    up

--- resources/views/sales/memo.blade.php
+++ resources/views/sales/memo.blade.php
@@ -4,7 +4,8 @@
 @section('pageTitle',trans('Sales Reports'))
 @section('pageSubTitle',trans('Reports'))
 @section('content')
-<section id="multiple-column-form">
+<link href="https://fonts.maateen.me/adorsho-lipi/font.css" rel="stylesheet">
+<section id="multiple-column-form" style="font-family: 'AdorshoLipi', sans-serif;">
     <div class="row match-height">
         <div class="col-12">
             <div class="card">
@@ -19,102 +20,102 @@
                             border-collapse: collapse;
                         }
                     </style>
-                <table width="100%">
-                    <tr>
-                        <th class="text-center" style="font-size: 40px; font-weight: 900;">
-                            মেসার্স রামপুর সিন্ডিকেট
-                        </th>
-                    </tr>
-                </table>
-                <table width="100%">
-                    <tr>
-                        <th width="41%"></th>
-                        <th width="18%"
-                            style="font-size: 20px; background-color: rgb(70, 70, 230); color: white;  border-radius: 5px; text-align:center">
-                            কমিশন এজেন্ট
-                        </th>
-                        <th></th>
-                    </tr>
-                </table>
-                <table width="100%">
-                    <tr>
-                        <td style="font-size: 18px; text-align: center;">
-                            ১৯৩, খাতুনগঞ্জ, চট্টগ্রাম, বাংলাদেশ।
-                        </td>
+                    <table width="100%">
+                        <tr>
+                            <th class="text-center" style="font-size: 40px; font-weight: 900;">
+                                মেসার্স রামপুর সিন্ডিকেট
+                            </th>
+                        </tr>
+                    </table>
+                    <table width="100%">
+                        <tr>
+                            <th width="41%"></th>
+                            <th width="18%"
+                                style="font-size: 20px; background-color: rgb(70, 70, 230); color: white;  border-radius: 5px; text-align:center">
+                                কমিশন এজেন্ট
+                            </th>
+                            <th></th>
+                        </tr>
+                    </table>
+                    <table width="100%">
+                        <tr>
+                            <td style="font-size: 18px; text-align: center;">
+                                ১৯৩, খাতুনগঞ্জ, চট্টগ্রাম, বাংলাদেশ।
+                            </td>
 
-                    </tr>
-                </table>
-                <table width="100%">
-                    <tr>
-                        <td style="font-size: 18px; text-align: center;">
-                            মোবাইলঃ ০১৮১৯-৩৭৭৩৭২, ০১৬৭২-৯৮১৬১৪,০১৭৫৮-৯৮২৬৬১
-                        </td>
-                    </tr>
-                </table>
-                <table width="100%">
-                    <tr>
-                        <td colspan="3" style="width: 28%; "> নং </td>
-                        <td style="width: 44%;"></td>
-                        <td>তারিখ_________{{ date('d-M-Y', strtotime($show_data->sales_date)) }}_____________________</td>
-                    </tr>
-                </table>
-                <table width="100%">
-                    <tr>
-                        <td>
-                            নাম______________{{$show_data->customer?->customer_name}}_____________________________________________________________________________________
-                        </td>
-                    </tr>
-                </table>
-                <table width="100%">
-                    <tr>
-                        <td>
-                            ঠিকানা_____________{{$show_data->customer?->address}}____________________________________________________________________________________
-                        </td>
-                    </tr>
-                </table>
-                <br>
-                <table width="100%" border="1" cellspacing="0"
-                    style="background-color: rgb(223, 196, 248);border-color:rgb(187, 134, 237) ;">
-                    <tr class="text-center" style="color: rgb(126, 126, 240); position: relative;">
-                        <th width="70%" style="background-color: rgb(223, 196, 248)">বিবরণ</th>
-                        <th width="10%">দর</th>
-                        <th width="20%">টাকা</th>
-                    </tr>
-                    @php
-                    $totalAmount = 0;
-                    @endphp
-                    @forelse($salesDetail as $s)
-                    <tr class="text-center">
-                        <td width="70%" style="background-color: aliceblue;">{{$s->product?->product_name}}</td>
-                        <td width="10%">{{$s->rate_kg}}</td>
-                        <td width="20%">{{$s->amount}}</td>
-                    </tr>
-                    @php
-                    $totalAmount += $s->amount;
-                    @endphp
-                    <tr style="height: 10px">
-                        <td width="70%" style="background-color: aliceblue;"></td>
-                        <td width="10%"></td>
-                        <td width="20%"></td>
-                    </tr>
-                    @empty
-                    <tr>
-                        <th colspan="10">No data Found</th>
-                    </tr>
-                    @endforelse
-                    <tr class="text-center">
-                        <td style="text-align: right;">মোট </td>
-                        <td></td>
-                        <td>{{ $totalAmount }}</td>
-                    </tr>
-                </table>
-                <div
-                    {{--  style="transform: rotate(-35deg); position: absolute; margin-top: -280px; font-size: 35px; opacity: 0.5; padding-left: 300px;">
-                    কমিশন
-                    ভিত্তিক</div>  --}}
-                <div style="text-align: center; font-size: 20px;"><b> ধন্যবাদ আবার আসবেন।</b></div>
+                        </tr>
+                    </table>
+                    <table width="100%">
+                        <tr>
+                            <td style="font-size: 18px; text-align: center;">
+                                মোবাইলঃ ০১৮১৯-৩৭৭৩৭২, ০১৬৭২-৯৮১৬১৪,০১৭৫৮-৯৮২৬৬১
+                            </td>
+                        </tr>
+                    </table>
+                    <table width="100%">
+                        <tr>
+                            <td colspan="3" style="width: 28%; "> নং </td>
+                            <td style="width: 44%;"></td>
+                            <td>তারিখ_________{{ date('d-M-Y', strtotime($show_data->sales_date)) }}_____________________</td>
+                        </tr>
+                    </table>
+                    <table width="100%">
+                        <tr>
+                            <td>
+                                নাম______________{{$show_data->customer?->customer_name}}_____________________________________________________________________________________
+                            </td>
+                        </tr>
+                    </table>
+                    <table width="100%">
+                        <tr>
+                            <td>
+                                ঠিকানা_____________{{$show_data->customer?->address}}____________________________________________________________________________________
+                            </td>
+                        </tr>
+                    </table>
+                    <br>
+                    <table width="100%" border="1" cellspacing="0">
+                        <tr class="text-center" style="">
+                            <th width="70%">বিবরণ</th>
+                            <th width="10%">দর</th>
+                            <th width="20%">টাকা</th>
+                        </tr>
+                        @php
+                        $totalAmount = 0;
+                        @endphp
+                        @forelse($salesDetail as $s)
+                        <tr class="text-center">
+                            <td width="70%">{{$s->product?->product_name}}</td>
+                            <td width="10%">{{$s->rate_kg}}</td>
+                            <td width="20%">{{$s->amount}}</td>
+                        </tr>
+                        @php
+                        $totalAmount += $s->amount;
+                        @endphp
+                        <tr style="height: 10px">
+                            <td width="70%"></td>
+                            <td width="10%"></td>
+                            <td width="20%"></td>
+                        </tr>
+                        @empty
+                        <tr>
+                            <th colspan="10">No data Found</th>
+                        </tr>
+                        @endforelse
+                        <tr style="height: 300px;"></tr>
+                        <tr class="text-center">
+                            <td style="text-align: right;">মোট </td>
+                            <td></td>
+                            <td>{{ $totalAmount }}</td>
+                        </tr>
+                    </table>
+                    <div
+                        {{--  style="transform: rotate(-35deg); position: absolute; margin-top: -280px; font-size: 35px; opacity: 0.5; padding-left: 300px;">
+                        কমিশন
+                        ভিত্তিক</div>  --}}
+                    <div style="text-align: center; font-size: 20px;"><b> ধন্যবাদ আবার আসবেন।</b></div>
 
-                <div style="text-align: right; padding-right: 100px;">স্বাক্ষর</div>
+                    <div style="text-align: right; padding-right: 100px;">স্বাক্ষর</div>
             </div>
         </div>
     </div>
