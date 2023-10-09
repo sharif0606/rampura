93eda42b96245abfdda2a4cd8b3713a3daa19cfb
Nasim Ahmed <nashim.idb333@gmail.com>
2023-10-09 17:11:00 +0600

    update sales and purchase pages

--- resources/views/sales/create.blade.php
+++ resources/views/sales/create.blade.php
@@ -139,7 +139,7 @@
                                                     <select name="child_two_id[]" class="form-select">
                                                         <option value="">select</option>
                                                         @forelse ($childTow as $ex)
-                                                            <option value="{{$ex->id}}">{{$ex->head_name}}</option>
+                                                            <option value="{{$ex->id}} {{old('child_two_id[]')==$ex->id?'selected':''}}">{{$ex->head_name}}</option>
                                                         @empty
                                                             <option value="">No Data Found</option>
                                                         @endforelse
@@ -148,8 +148,8 @@
                                                 <td class="tbl_expense"><input type="text" onblur="checking_lc_no(this)" class="form-control" name="lc_no[]" placeholder="Lc Number"><span class="error-message" style="color: red; display: none;"></span></td>
                                                 <td class="tbl_expense">
                                                     <select name="sign_for_calculate[]" class="form-select">
-                                                        <option value="+" {{old('sign_for_calculate')== '+'?'selected':''}}>(+)</option>
-                                                        <option value="-" {{old('sign_for_calculate')== '-'?'selected':''}}>(-)</option>
+                                                        <option value="+" {{old('sign_for_calculate[]')== '+'?'selected':''}}>(+)</option>
+                                                        <option value="-" {{old('sign_for_calculate[]')== '-'?'selected':''}}>(-)</option>
                                                     </select>
                                                 </td>
                                                 <td class="tbl_expense"><input type="number" onkeyup="total_expense(this)" class="form-control expense_value text-end" name="cost_amount[]"></td>
@@ -410,7 +410,7 @@ var row=`<tr class="tbl_expense">
                 <select required name="child_two_id[]" class="form-select">
                     <option value="">select</option>
                     @forelse ($childTow as $ex)
-                        <option value="{{$ex->id}}">{{$ex->head_name}}</option>
+                        <option value="{{$ex->id}} {{old('child_two_id[]')==$ex->id?'selected':''}}">{{$ex->head_name}}</option>
                     @empty
                         <option value="">No Data Found</option>
                     @endforelse
@@ -419,8 +419,8 @@ var row=`<tr class="tbl_expense">
             <td class="tbl_expense"><input type="text" onblur="checking_lc_no(this)" class="form-control" name="lc_no[]" placeholder="Lc Number" required><span class="error-message" style="color: red; display: none;"></span></td>
             <td class="tbl_expense">
                 <select name="sign_for_calculate[]" class="form-select">
-                    <option value="+" {{old('sign_for_calculate')== '+'?'selected':''}}>(+)</option>
-                    <option value="-" {{old('sign_for_calculate')== '-'?'selected':''}}>(-)</option>
+                    <option value="+" {{old('sign_for_calculate[]')== '+'?'selected':''}}>(+)</option>
+                    <option value="-" {{old('sign_for_calculate[]')== '-'?'selected':''}}>(-)</option>
                 </select>
             </td>
             <td class="tbl_expense"><input type="number" onkeyup="total_expense(this)" class="form-control expense_value text-end" name="cost_amount[]"></td>
