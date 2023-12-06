e2b3365b00fd4c4ca790f29749ac6ac0cdde0a6a
Nasim Ahmed <nashim.idb333@gmail.com>
2023-12-06 21:13:03 +0600

    dd off updated

--- app/Http/Controllers/Settings/ProfileController.php
+++ app/Http/Controllers/Settings/ProfileController.php
@@ -75,7 +75,7 @@ class ProfileController extends Controller
             else
                 return redirect()->back()->withInput()->with($this->resMessageHtml(false,'error','Please try again'));
         }catch(Exception $e){
-            dd($e);
+            // dd($e);
             return redirect()->back()->withInput()->with($this->resMessageHtml(false,'error','Please try again'));
         }
     }
