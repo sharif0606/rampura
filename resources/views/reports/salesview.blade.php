b3bcaec7f57775455314825b3fb143c0858dae1a
Nasim Ahmed <nashim.idb333@gmail.com>
2023-12-27 20:46:00 +0600

    update sales report

--- resources/views/reports/salesview.blade.php
+++ resources/views/reports/salesview.blade.php
@@ -4,6 +4,13 @@
 @section('pageSubTitle',trans('Reports'))
 
 @section('content')
+<style>
+
+    .tbl_border{
+    border: 1px solid;
+    border-collapse: collapse;
+    }
+</style>
 <section id="multiple-column-form">
     <div class="row match-height">
         <div class="col-12">
@@ -64,18 +71,18 @@
                             <div class="table-responsive">
                                 <table class="table mb-5">
                                     <thead>
-                                        <tr class="bg-primary text-white text-center">
-                                            <th class="p-2">{{__('#SL')}}</th>
-                                            <th class="p-2" data-title="Party Name">{{__('Party Name')}}</th>
-                                            <th class="p-2" data-title="Description of Goods">{{__('Des.of Goods')}}</th>
-                                            <th class="p-2" data-title="LC / LOT NO">{{__('Lc/Lot no')}}</th>
-                                            <th class="p-2" data-title="TRADEMARKE/BRAND NAME">{{__('Trade/Brand')}}</th>
-                                            <th class="p-2" data-title="Quantity Bag">{{__('Q.bag')}}</th>
-                                            <th class="p-2" data-title="Total Quantity in Kg">{{__('Total Kg')}}</th>
-                                            <th class="p-2" data-title="Rate in Kg">{{__('Rate in kg')}}</th>
-                                            <th class="p-2" data-title="Amount">{{__('Amount')}}</th>
-                                            {{-- <th class="p-2" data-title="Sales Commission Income">{{__('S.Com')}}</th>
-                                            <th class="p-2" data-title="Total Amount">{{__('Total Amount')}}</th> --}}
+                                        <tr class="tbl_border bg-primary text-white text-center">
+                                            <th class="p-2 tbl_border">{{__('Date')}}</th>
+                                            <th class="p-2 tbl_border">{{__('Particulars')}}</th>
+                                            <th class="p-2 tbl_border">{{__('Voucher Type')}}</th>
+                                            <th class="p-2 tbl_border">{{__('Quantity')}}</th>
+                                            <th class="p-2 tbl_border">{{__('Alt.Unints')}}</th>
+                                            <th class="p-2 tbl_border">{{__('Rate')}}</th>
+                                            <th class="p-2 tbl_border">{{__('Gross Total')}}</th>
+                                            <th class="p-2 tbl_border"> Sales- {{encryptor('decrypt', request()->session()->get('companyName'))}}</th>
+                                            <th class="p-2 tbl_border">{{__('Commission')}}</th>
+                                            <th class="p-2 tbl_border">{{__('Loading Charge')}}</th>
+                                            <th class="p-2 tbl_border">{{__('Shopi Mazi')}}</th>
                                         </tr>
                                     </thead>
                                     <tbody>
@@ -87,18 +94,42 @@
                                             // $totalAmount = 0;
                                         @endphp
                                         @forelse($data as $d)
-                                        <tr class="text-center">
-                                            <th scope="row">{{ ++$loop->index }}</th>
-                                            <td>{{$d->customer?->customer_name}}</td>
-                                            <td>{{$d->product?->product_name}}</td>
-                                            <td>{{$d->lot_no}}</td>
-                                            <td>{{$d->brand}}</td>
-                                            <td>{{money_format($d->quantity_bag)}}</td>
-                                            <td>{{money_format($d->quantity_kg)}}</td>
-                                            <td>{{$d->rate_kg}}</td>
-                                            <td>{{money_format($d->amount)}}</td>
-                                            {{-- <td>{{$d->sale_commission}}</td>
-                                            <td>{{$d->total_amount}}</td> --}}
+                                        <tr class="tbl_border text-center">
+                                            <td class="tbl_border" scope="row">{{ $d->sales_date }}</td>
+                                            <td class="tbl_border">{{$d->customer?->customer_name}}</td>
+                                            <td class="tbl_border"></td>
+                                            <td class="tbl_border"><b>{{money_format($d->quantity_kg)}} কেজি</b></td>
+                                            <td class="tbl_border">{{money_format($d->quantity_bag)}} বস্তা</td>
+                                            <td class="tbl_border"></td>
+                                            <td class="tbl_border">
+                                                @if ($d->grand_total != '')
+                                                    {{money_format($d->grand_total)}} Dr
+                                                @else
+                                                @endif
+                                            </td>
+                                            <td class="tbl_border">
+                                                @if ($d->amount != '')
+                                                    {{money_format($d->amount)}} Cr
+                                                @else
+                                                @endif
+                                                
+                                            </td>
+                                            <td class="tbl_border"></td>
+                                            <td class="tbl_border"></td>
+                                            <td class="tbl_border"></td>
+                                        </tr>
+                                        <tr class="tbl_border text-center">
+                                            <td class="tbl_border"></td>
+                                            <td class="tbl_border">{{$d->product?->product_name}}--{{$d->lot_no}}--{{$d->brand}}</td>
+                                            <td class="tbl_border"></td>
+                                            <td class="tbl_border">{{money_format($d->quantity_kg)}} কেজি</td>
+                                            <td class="tbl_border">{{money_format($d->quantity_bag)}} বস্তা</td>
+                                            <td class="tbl_border">{{$d->rate_kg}}</td>
+                                            <td class="tbl_border"></td>
+                                            <td class="tbl_border"></td>
+                                            <td class="tbl_border"></td>
+                                            <td class="tbl_border"></td>
+                                            <td class="tbl_border"></td>
                                         </tr>
                                         @php
                                             $totalBagQty += $d->quantity_bag;
@@ -109,21 +140,19 @@
                                         @endphp
                                         @empty
                                         <tr>
-                                            <th colspan="9" class="text-center">No data Found</th>
+                                            <th colspan="11" class="text-center">No data Found</th>
                                         </tr>
                                         @endforelse
                                     </tbody>
-                                    <tfoot>
+                                    {{-- <tfoot>
                                         <tr class="text-center">
                                             <th colspan="5" class="p-2">Total</th>
                                             <th class="p-2">{{money_format($totalBagQty)}}</th>
                                             <th class="p-2">{{money_format($totalQty)}}</th>
                                             <th class="p-2"></th>
                                             <th class="p-2">{{money_format($amount)}}</th>
-                                            {{-- <th class="p-2">{{$saleCommission}}</th>
-                                            <th class="p-2">{{$totalAmount}}</th> --}}
                                         </tr>
-                                    </tfoot>
+                                    </tfoot> --}}
                                 </table>
                             </div>
                         </form>
