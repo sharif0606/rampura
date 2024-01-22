1f9c9b5c346542c297cb37c93958f2c769922bc1
Nasim Ahmed <nashim.idb333@gmail.com>
2024-01-22 20:52:08 +0600

    update excutive dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -70,6 +70,22 @@ class DashboardController extends Controller
     * sales man dashboard
     */
     public function salesmanDashboard(){
+        $ldate = new DateTime('today');
+
+        $supplier = Supplier::where(company())->count();
+        $customer = Customer::where('is_walking',0)->where(company())->count();
+        $totalPurchase = Purchase::where(company())->count();
+        $totalRegularPurchase = Regular_purchase::where(company())->count();
+        $totalBeparianPurchase = Beparian_purchase::where(company())->count();
+        $totalPurchaseAmount = Purchase::where(company())->sum('grand_total');
+        $totalRegularPurchaseAmount = Regular_purchase::where(company())->sum('grand_total');
+        $totalBeparianPurchaseAmount = Beparian_purchase::where(company())->sum('grand_total');
+        $todayPurchaseAmount = Purchase::where('purchase_date',$ldate->format('Y-m-d'))->where(company())->sum('grand_total');
+        $todayRegularPurchaseAmount = Regular_purchase::where('purchase_date',$ldate->format('Y-m-d'))->where(company())->sum('grand_total');
+        $todayBeparianPurchaseAmount = Beparian_purchase::where('purchase_date',$ldate->format('Y-m-d'))->where(company())->sum('grand_total');
+        $totalSale = Sales::where(company())->count();
+        $totalSaleAmount = Sales::where(company())->sum('grand_total');
+        $todayTotalSaleAmount = Sales::where('sales_date',$ldate->format('Y-m-d'))->where(company())->sum('grand_total');
         return view('dasbhoard.salesman',compact('supplier','customer','totalPurchase','totalBeparianPurchase','totalRegularPurchase','totalSale','totalPurchaseAmount','totalRegularPurchaseAmount','totalBeparianPurchaseAmount','totalSaleAmount','todayTotalSaleAmount','todayPurchaseAmount','todayRegularPurchaseAmount','todayBeparianPurchaseAmount'));
     }
 
@@ -77,6 +93,22 @@ class DashboardController extends Controller
     * sales man dashboard
     */
     public function executiveDashboard(){
+        $ldate = new DateTime('today');
+
+        $supplier = Supplier::where(company())->count();
+        $customer = Customer::where('is_walking',0)->where(company())->count();
+        $totalPurchase = Purchase::where(company())->count();
+        $totalRegularPurchase = Regular_purchase::where(company())->count();
+        $totalBeparianPurchase = Beparian_purchase::where(company())->count();
+        $totalPurchaseAmount = Purchase::where(company())->sum('grand_total');
+        $totalRegularPurchaseAmount = Regular_purchase::where(company())->sum('grand_total');
+        $totalBeparianPurchaseAmount = Beparian_purchase::where(company())->sum('grand_total');
+        $todayPurchaseAmount = Purchase::where('purchase_date',$ldate->format('Y-m-d'))->where(company())->sum('grand_total');
+        $todayRegularPurchaseAmount = Regular_purchase::where('purchase_date',$ldate->format('Y-m-d'))->where(company())->sum('grand_total');
+        $todayBeparianPurchaseAmount = Beparian_purchase::where('purchase_date',$ldate->format('Y-m-d'))->where(company())->sum('grand_total');
+        $totalSale = Sales::where(company())->count();
+        $totalSaleAmount = Sales::where(company())->sum('grand_total');
+        $todayTotalSaleAmount = Sales::where('sales_date',$ldate->format('Y-m-d'))->where(company())->sum('grand_total');
         return view('dasbhoard.executive',compact('supplier','customer','totalPurchase','totalBeparianPurchase','totalRegularPurchase','totalSale','totalPurchaseAmount','totalRegularPurchaseAmount','totalBeparianPurchaseAmount','totalSaleAmount','todayTotalSaleAmount','todayPurchaseAmount','todayRegularPurchaseAmount','todayBeparianPurchaseAmount'));
     }
 }
