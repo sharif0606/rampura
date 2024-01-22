5bf37cf4b958cc1cab1dc452e6ec4f78f42a65a7
Nasim Ahmed <nashim.idb333@gmail.com>
2024-01-22 20:43:37 +0600

    update owner dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Customers\Customer;
+use App\Models\Purchases\Beparian_purchase;
+use App\Models\Purchases\Purchase;
+use App\Models\Purchases\Regular_purchase;
+use App\Models\Sales\Sales;
+use App\Models\Suppliers\Supplier;
+use DateTime;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -17,7 +24,23 @@ class DashboardController extends Controller
     * owner dashboard
     */
     public function ownerDashboard(){
-        return view('dasbhoard.owner');
+        $ldate = new DateTime('today');
+
+        $supplier = Supplier::where(company())->count();
+        $customer = Customer::where('is_walking',0)->where(company())->count();
+        $totalPurchase = Purchase::where(company())->count();
+        $totalRegularPurchase = Regular_purchase::where(company())->count();
+        $totalBeparianPurchase = Beparian_purchase::where(company())->count();
+        $totalPurchaseAmount = Purchase::where(company())->sum('grand_total');
+        $totalRegularPurchaseAmount = Regular_purchase::where(company())->sum('grand_total');
+        $totalBeparianPurchaseAmount = Beparian_purchase::where(company())->sum('grand_total');
+        $todayPurchaseAmount = Purchase::where('purchase_date',$ldate->format('Y-m-d'))->where(company())->sum('grand_total');
+        $todayRegularPurchaseAmount = Regular_purchase::where('purchase_date',$ldate->format('Y-m-d'))->where(company())->sum('grand_total');
+        $todayBeparianPurchaseAmount = Beparian_purchase::where('purchase_date',$ldate->format('Y-m-d'))->where(company())->sum('grand_total');
+        $totalSale = Sales::where(company())->count();
+        $totalSaleAmount = Sales::where(company())->sum('grand_total');
+        $todayTotalSaleAmount = Sales::where('sales_date',$ldate->format('Y-m-d'))->where(company())->sum('grand_total');
+        return view('dasbhoard.owner',compact('supplier','customer','totalPurchase','totalBeparianPurchase','totalRegularPurchase','totalSale','totalPurchaseAmount','totalRegularPurchaseAmount','totalBeparianPurchaseAmount','totalSaleAmount','todayTotalSaleAmount','todayPurchaseAmount','todayRegularPurchaseAmount','todayBeparianPurchaseAmount'));
     }
     
     /*
--- resources/views/dasbhoard/owner.blade.php
+++ resources/views/dasbhoard/owner.blade.php
@@ -2,6 +2,11 @@
 @section('pageTitle',trans('Dashboard'))
 
 @section('content')
+@php
+    $purCount = 0;
+    $purAmount = 0;
+    $todayPurAmount = 0;
+@endphp
 
 <div class="page-content py-3">
     <section class="row">
@@ -11,8 +16,11 @@
                 <i class="bi bi-bag icon"></i>
                 </span>
                 <div class="info-box-content">
-                    <span class="text-bold text-uppercase">Total Purchase Due</span>
-                    <span class="info-box-number">৳  0.00</span>
+                    @php
+                        $purAmount = $totalPurchaseAmount + $totalRegularPurchaseAmount + $totalBeparianPurchaseAmount;
+                    @endphp
+                    <span class="text-bold text-uppercase">Total Purchase</span><br>
+                    <span class="info-box-number">৳  {{money_format($purAmount)}}</span>
                 </div>
             </div>
        </div>
@@ -33,8 +41,8 @@
                 <i class="bi bi-cart icon"></i>
                 </span>
                 <div class="info-box-content">
-                    <span class="text-bold text-uppercase">Total Sales Amount</span>
-                    <span class="info-box-number">৳  0.00</span>
+                    <span class="text-bold text-uppercase">Total Sales Amount</span><br>
+                    <span class="info-box-number">৳  {{money_format($totalSaleAmount)}}</span>
                 </div>
             </div>
        </div>
@@ -57,8 +65,11 @@
                 <i class="bi bi-bag icon"></i>
                 </span>
                 <div class="info-box-content">
-                    <span class="text-bold text-uppercase">Todays Total Purchase</span>
-                    <span class="info-box-number">৳  0.00</span>
+                    @php
+                        $todayPurAmount = $todayPurchaseAmount + $todayRegularPurchaseAmount + $todayBeparianPurchaseAmount;
+                    @endphp
+                    <span class="text-bold text-uppercase">Todays Total Purchase</span><br>
+                    <span class="info-box-number">৳  {{money_format($todayPurAmount)}}</span>
                 </div>
             </div>
        </div>
@@ -79,8 +90,8 @@
                 <i class="bi bi-cart icon"></i>
                 </span>
                 <div class="info-box-content">
-                    <span class="text-bold text-uppercase">Todays Total Sales</span>
-                    <span class="info-box-number">৳  0.00</span>
+                    <span class="text-bold text-uppercase">Todays Total Sales</span><br>
+                    <span class="info-box-number">৳  {{money_format($todayTotalSaleAmount)}}</span>
                 </div>
             </div>
        </div>
@@ -100,13 +111,13 @@
        <div class="col-md-3 col-sm-6 col-lg-3">
             <div class="small-box bg-dream-pink">
                <div class="inner text-uppercase">
-                    <h3>138</h3>
+                    <h3>{{$customer}}</h3>
                     <p>Customers</p>
                </div> 
                <div class="icon">
                 <i class="bi bi-people-fill"></i>
                </div>
-               <a href="#" class="small-box-footer text-uppercase">View
+               <a href="{{route(currentUser().'.customer.index')}}" class="small-box-footer text-uppercase">View
                 <i class="bi bi-arrow-right-circle"></i>
                </a>
             </div>
@@ -114,13 +125,13 @@
        <div class="col-md-3 col-sm-6 col-lg-3">
             <div class="small-box bg-dream-purple">
                <div class="inner text-uppercase">
-                    <h3>18</h3>
+                    <h3>{{$supplier}}</h3>
                     <p>Suppliers</p>
                </div> 
                <div class="icon">
                 <i class="bi bi-people-fill"></i>
                </div>
-               <a href="#" class="small-box-footer text-uppercase">View
+               <a href="{{route(currentUser().'.supplier.index')}}" class="small-box-footer text-uppercase">View
                 <i class="bi bi-arrow-right-circle"></i>
                </a>
             </div>
@@ -128,13 +139,16 @@
        <div class="col-md-3 col-sm-6 col-lg-3">
             <div class="small-box bg-dream-maroon">
                <div class="inner text-uppercase">
-                    <h3>18</h3>
+                @php
+                    $purCount = $totalPurchase + $totalBeparianPurchase + $totalRegularPurchase;
+                @endphp
+                    <h3>{{money_format($purCount)}}</h3>
                     <p>Purchase Invoice</p>
                </div> 
                <div class="icon">
                 <i class="bi bi-receipt"></i>
                </div>
-               <a href="#" class="small-box-footer text-uppercase">View
+               <a href="{{route(currentUser().'.purchase.index')}}" class="small-box-footer text-uppercase">View
                 <i class="bi bi-arrow-right-circle"></i>
                </a>
             </div>
@@ -142,13 +156,13 @@
        <div class="col-md-3 col-sm-6 col-lg-3">
             <div class="small-box bg-dream-green">
                <div class="inner text-uppercase">
-                    <h3>198</h3>
+                    <h3>{{$totalSale}}</h3>
                     <p>Sales Invoice</p>
                </div> 
                <div class="icon">
                 <i class="bi bi-receipt"></i>
                </div>
-               <a href="#" class="small-box-footer text-uppercase">View
+               <a href="{{route(currentUser().'.sales.index')}}" class="small-box-footer text-uppercase">View
                 <i class="bi bi-arrow-right-circle"></i>
                </a>
             </div>
