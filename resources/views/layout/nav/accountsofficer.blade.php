bf00d61296a5232ec4281b5b7bcc5bc28bb80d5e
Nasim Ahmed <nashim.idb333@gmail.com>
2024-01-30 14:22:58 +0600

    update sidebar menu

--- resources/views/layout/nav/accountsofficer.blade.php
+++ resources/views/layout/nav/accountsofficer.blade.php
@@ -9,7 +9,121 @@
             <span>{{__('dashboard') }}</span>
         </a>
     </li>
+    <li class="sidebar-item has-sub">
+        <a href="#" class='sidebar-link'><i class="bi bi-cart-fill"></i><span>{{__('Sales')}}</span></a>
+        <ul class="submenu">
+            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.sales.index')}}">{{__('Sales List')}}</a></li>
+            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.sales.create')}}">{{__('New Sales')}}</a></li>
+            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.sales.cash')}}">{{__('Cash Sales')}}</a></li>
+            {{-- <li class="py-1 submenu-item"><a href="{{route(currentUser().'.sales_pending_exp')}}">{{__('Pending Expense')}}</a></li> controller and view can be deleted --}} 
+            {{-- <li class="py-1 submenu-item"><a href="{{route(currentUser().'.sales_pending_pay')}}">{{__('Pending Payment')}}</a></li> --}}
+		</ul>   
+    </li>
+
+    <li class="sidebar-item has-sub">
+        <a href="#" class='sidebar-link'><i class="bi bi-cart-plus-fill"></i><span>{{__('Purchases')}}</span></a>
+        <ul class="submenu">
+            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.purchase.index')}}">{{__('Purchase')}}</a></li>
+            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.bpurchase.index')}}">{{__('Beparian Purchase')}}</a></li>
+            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.rpurchase.index')}}">{{__('Regular Purchase')}}</a></li>
+            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.pur_pending_exp')}}">{{__('Pending Expense')}}</a></li>
+            {{-- <li class="py-1 submenu-item"><a href="{{route(currentUser().'.pur_pending_pay')}}">{{__('Pending Payment')}}</a></li> --}}
+		</ul>   
+    </li>
+    <li class="sidebar-item has-sub">
+        <a href="#" class='sidebar-link'><i class="bi bi-receipt"></i><span>{{__('Voucher')}}</span>
+        </a>
+        <ul class="submenu">
+            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.credit.index')}}">{{__('Receive/Cr Voucher')}}</a></li>
+            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.debit.index')}}">{{__('Payment/Dr Voucher')}}</a></li>
+            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.journal.index')}}">{{__('Journal Voucher')}}</a></li>
+            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.purchase_voucher.index')}}">{{__('Purchase Voucher')}}</a></li>
+            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.sales_voucher.index')}}">{{__('Sales Voucher')}}</a></li>
+        </ul>
+    </li>
     
+    <li class="sidebar-item has-sub">
+        <a href="#" class='sidebar-link'><i class="bi bi-people-fill"></i></i><span>{{__('Customer')}}</span></a>
+        <ul class="submenu">
+            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.customer.index')}}">{{__('Customer List')}}</a></li>
+            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.customer.create')}}">{{__('New Customer')}}</a></li>
+		</ul>   
+    </li>
+
+    <li class="sidebar-item has-sub">
+        <a href="#" class='sidebar-link'><i class="bi bi-people-fill"></i></i><span>{{__('Supplier')}}</span></a>
+        <ul class="submenu">
+            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.supplier.index')}}">{{__('Supplier List')}}</a></li>
+            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.supplier.create')}}">{{__('New Supplier')}}</a></li>
+		</ul>   
+    </li>
+    
+    
+    <li class="sidebar-item has-sub">
+        <a href="#" class='sidebar-link'><i class="bi bi-box-fill"></i><span>{{__('Products')}}</span>
+        </a>
+        <ul class="submenu">
+            <li class="submenu-item sidebar-item has-sub"><a href="#" class='sidebar-link'> {{__('Category')}}</a>
+                <ul class="submenu">
+                    <li class="py-1 submenu-item"><a href="{{route(currentUser().'.category.index')}}">{{__('Category List')}}</a></li>
+                    <li class="py-1 submenu-item"><a href="{{route(currentUser().'.category.create')}}">{{__('New Category')}}</a></li>
+                </ul>
+            </li>
+            <li class="submenu-item sidebar-item has-sub"><a href="#" class='sidebar-link'>{{__('Sub Category')}}</a>
+                <ul class="submenu">
+                    <li class="py-1 submenu-item"><a href="{{route(currentUser().'.subcategory.index')}}">{{__('Sub Category List')}}</a></li>
+                    <li class="py-1 submenu-item"><a href="{{route(currentUser().'.subcategory.create')}}">{{__('New Sub Category')}}</a></li>
+                </ul>
+            </li>
+            {{-- <li class="submenu-item sidebar-item has-sub"><a href="#" class='sidebar-link'>{{__('Child Category')}}</a>
+                <ul class="submenu">
+                    <li class="py-1 submenu-item"><a href="{{route(currentUser().'.childcategory.index')}}">{{__('List')}}</a></li>
+                    <li class="py-1 submenu-item"><a href="{{route(currentUser().'.childcategory.create')}}">{{__('Add New')}}</a></li>
+                </ul>
+            </li> --}}
+            {{-- <li class="submenu-item sidebar-item has-sub"><a href="#" class='sidebar-link'>{{__('Brand')}}</a>
+                <ul class="submenu">
+                    <li class="py-1 submenu-item"><a href="{{route(currentUser().'.brand.index')}}">{{__('List')}}</a></li>
+                    <li class="py-1 submenu-item"><a href="{{route(currentUser().'.brand.create')}}">{{__('Add New')}}</a></li>
+                </ul>
+            </li> --}}
+            <li class="submenu-item sidebar-item has-sub"><a href="#" class='sidebar-link'>{{__('Products')}}</a>
+                <ul class="submenu">
+                    <li class="py-1 submenu-item"><a href="{{route(currentUser().'.product.index')}}">{{__('Product List')}}</a></li>
+                    <li class="py-1 submenu-item"><a href="{{route(currentUser().'.product.create')}}">{{__('New Product')}}</a></li>
+                </ul>
+            </li>
+            {{-- <li><a href="{{route(currentUser().'.plabel')}}" >{{__('Product Label')}}</a></li> --}}
+		</ul>
+    </li>
+    <li class="sidebar-item has-sub">
+        <a href="#" class='sidebar-link'><i class="bi bi-card-checklist"></i><span>{{__('Report')}}</span></a>
+        <ul class="submenu">
+            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.purchase_report')}}" >{{__('Purchase Report')}}</a></li>
+            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.beparian_report')}}" >{{__('Beparian Report')}}</a></li>
+            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.regular_report')}}" >{{__('Regular Report')}}</a></li>
+            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.all_pur_report')}}" >{{__('Purchase All')}}</a></li>
+            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.salreport')}}" >{{__('Sales Report')}}</a></li>
+            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.salreport_account')}}" >{{__('Sales Report With AC')}}</a></li>
+            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.sreport')}}" >{{__('Stock Report')}}</a></li>
+            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.srota')}}" >{{__('Srota')}}</a></li>
+            <li class="text-bold py-2 border-bottom ">Accounts Report</li>
+            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.incomeStatement')}}">{{__('Income Statement')}}</a></li>
+            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.headreport')}}" >{{__('Account Head Report')}}</a></li>
+            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.lc_report')}}" >{{__('LC Expense Report')}}</a></li>
+        </ul>
+    </li>
+    <li class="sidebar-item has-sub">
+        <a href="#" class='sidebar-link'><i class="bi bi-calculator"></i><span>{{__('Accounts')}}</span>
+        </a>
+        <ul class="submenu">
+            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.master.index')}}" >{{__('Master Head')}}</a></li>
+            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.sub_head.index')}}" >{{__('Sub Head')}}</a></li>
+            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.child_one.index')}}" >{{__('Child One')}}</a></li>
+            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.child_two.index')}}" >{{__('Child Two')}}</a></li>
+            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.navigate.index')}}">{{__('Navigate View')}}</a></li>
+		</ul>
+    </li>
     <li class="sidebar-item has-sub">
         <a href="#" class='sidebar-link'>
             <i class="bi bi-gear-fill"></i>
@@ -65,130 +179,5 @@
                 </ul>
             </li>
 		</ul>
-    </li>
-
-
-
-    <li class="sidebar-item has-sub">
-        <a href="#" class='sidebar-link'><i class="bi bi-box-fill"></i><span>{{__('Products')}}</span>
-        </a>
-        <ul class="submenu">
-            <li class="submenu-item sidebar-item has-sub"><a href="#" class='sidebar-link'> {{__('Category')}}</a>
-                <ul class="submenu">
-                    <li class="py-1 submenu-item"><a href="{{route(currentUser().'.category.index')}}">{{__('Category List')}}</a></li>
-                    <li class="py-1 submenu-item"><a href="{{route(currentUser().'.category.create')}}">{{__('New Category')}}</a></li>
-                </ul>
-            </li>
-            <li class="submenu-item sidebar-item has-sub"><a href="#" class='sidebar-link'>{{__('Sub Category')}}</a>
-                <ul class="submenu">
-                    <li class="py-1 submenu-item"><a href="{{route(currentUser().'.subcategory.index')}}">{{__('Sub Category List')}}</a></li>
-                    <li class="py-1 submenu-item"><a href="{{route(currentUser().'.subcategory.create')}}">{{__('New Sub Category')}}</a></li>
-                </ul>
-            </li>
-            {{-- <li class="submenu-item sidebar-item has-sub"><a href="#" class='sidebar-link'>{{__('Child Category')}}</a>
-                <ul class="submenu">
-                    <li class="py-1 submenu-item"><a href="{{route(currentUser().'.childcategory.index')}}">{{__('List')}}</a></li>
-                    <li class="py-1 submenu-item"><a href="{{route(currentUser().'.childcategory.create')}}">{{__('Add New')}}</a></li>
-                </ul>
-            </li> --}}
-            {{-- <li class="submenu-item sidebar-item has-sub"><a href="#" class='sidebar-link'>{{__('Brand')}}</a>
-                <ul class="submenu">
-                    <li class="py-1 submenu-item"><a href="{{route(currentUser().'.brand.index')}}">{{__('List')}}</a></li>
-                    <li class="py-1 submenu-item"><a href="{{route(currentUser().'.brand.create')}}">{{__('Add New')}}</a></li>
-                </ul>
-            </li> --}}
-            <li class="submenu-item sidebar-item has-sub"><a href="#" class='sidebar-link'>{{__('Products')}}</a>
-                <ul class="submenu">
-                    <li class="py-1 submenu-item"><a href="{{route(currentUser().'.product.index')}}">{{__('Product List')}}</a></li>
-                    <li class="py-1 submenu-item"><a href="{{route(currentUser().'.product.create')}}">{{__('New Product')}}</a></li>
-                </ul>
-            </li>
-            {{-- <li><a href="{{route(currentUser().'.plabel')}}" >{{__('Product Label')}}</a></li> --}}
-		</ul>
-    </li>
-
-    <li class="sidebar-item has-sub">
-        <a href="#" class='sidebar-link'><i class="bi bi-cart-plus-fill"></i><span>{{__('Purchases')}}</span></a>
-        <ul class="submenu">
-            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.purchase.index')}}">{{__('Purchase')}}</a></li>
-            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.bpurchase.index')}}">{{__('Beparian Purchase')}}</a></li>
-            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.rpurchase.index')}}">{{__('Regular Purchase')}}</a></li>
-            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.pur_pending_exp')}}">{{__('Pending Expense')}}</a></li>
-            {{-- <li class="py-1 submenu-item"><a href="{{route(currentUser().'.pur_pending_pay')}}">{{__('Pending Payment')}}</a></li> --}}
-		</ul>   
-    </li>
-
-    <li class="sidebar-item has-sub">
-        <a href="#" class='sidebar-link'><i class="bi bi-cart-fill"></i><span>{{__('Sales')}}</span></a>
-        <ul class="submenu">
-            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.sales.index')}}">{{__('Sales List')}}</a></li>
-            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.sales.create')}}">{{__('New Sales')}}</a></li>
-            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.sales.cash')}}">{{__('Cash Sales')}}</a></li>
-            {{-- <li class="py-1 submenu-item"><a href="{{route(currentUser().'.sales_pending_exp')}}">{{__('Pending Expense')}}</a></li> controller and view can be deleted --}} 
-            {{-- <li class="py-1 submenu-item"><a href="{{route(currentUser().'.sales_pending_pay')}}">{{__('Pending Payment')}}</a></li> --}}
-		</ul>   
-    </li>
-
-    
-    {{-- <li class="sidebar-item has-sub">
-        <a href="#" class='sidebar-link'><i class="bi bi-sign-turn-right-fill"></i><span>{{__('Transfer')}}</span></a>
-        <ul class="submenu">
-            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.transfer.index')}}">{{__('Transfer list')}}</a></li>
-            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.transfer.create')}}">{{__('Transfer')}}</a></li>
-		</ul>   
-    </li> --}}
-    <li class="sidebar-item has-sub">
-        <a href="#" class='sidebar-link'><i class="bi bi-people-fill"></i></i><span>{{__('Supplier')}}</span></a>
-        <ul class="submenu">
-            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.supplier.index')}}">{{__('Supplier List')}}</a></li>
-            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.supplier.create')}}">{{__('New Supplier')}}</a></li>
-		</ul>   
-    </li>
-    <li class="sidebar-item has-sub">
-        <a href="#" class='sidebar-link'><i class="bi bi-people-fill"></i></i><span>{{__('Customer')}}</span></a>
-        <ul class="submenu">
-            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.customer.index')}}">{{__('Customer List')}}</a></li>
-            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.customer.create')}}">{{__('New Customer')}}</a></li>
-		</ul>   
-    </li>
-
-    <li class="sidebar-item has-sub">
-        <a href="#" class='sidebar-link'><i class="bi bi-calculator"></i><span>{{__('Accounts')}}</span>
-        </a>
-        <ul class="submenu">
-            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.master.index')}}" >{{__('Master Head')}}</a></li>
-            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.sub_head.index')}}" >{{__('Sub Head')}}</a></li>
-            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.child_one.index')}}" >{{__('Child One')}}</a></li>
-            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.child_two.index')}}" >{{__('Child Two')}}</a></li>
-            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.navigate.index')}}">{{__('Navigate View')}}</a></li>
-		</ul>
-    </li>
-
-    <li class="sidebar-item has-sub">
-        <a href="#" class='sidebar-link'><i class="bi bi-receipt"></i><span>{{__('Voucher')}}</span>
-        </a>
-        <ul class="submenu">
-            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.credit.index')}}">{{__('Receive/Cr Voucher')}}</a></li>
-            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.debit.index')}}">{{__('Payment/Dr Voucher')}}</a></li>
-            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.journal.index')}}">{{__('Journal Voucher')}}</a></li>
-            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.purchase_voucher.index')}}">{{__('Purchase Voucher')}}</a></li>
-            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.sales_voucher.index')}}">{{__('Sales Voucher')}}</a></li>
-        </ul>
-    </li>
-    <li class="sidebar-item has-sub">
-        <a href="#" class='sidebar-link'><i class="bi bi-card-checklist"></i><span>{{__('Report')}}</span></a>
-        <ul class="submenu">
-            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.purchase_report')}}" >{{__('Purchase Report')}}</a></li>
-            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.beparian_report')}}" >{{__('Beparian Report')}}</a></li>
-            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.regular_report')}}" >{{__('Regular Report')}}</a></li>
-            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.all_pur_report')}}" >{{__('Purchase All')}}</a></li>
-            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.salreport')}}" >{{__('Sales Report')}}</a></li>
-            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.sreport')}}" >{{__('Stock Report')}}</a></li>
-            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.srota')}}" >{{__('Srota')}}</a></li>
-            <li class="text-bold py-2 border-bottom ">Accounts Report</li>
-            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.incomeStatement')}}">{{__('Income Statement')}}</a></li>
-            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.headreport')}}" >{{__('Account Head Report')}}</a></li>
-            <li class="py-1 submenu-item"><a href="{{route(currentUser().'.lc_report')}}" >{{__('LC Expense Report')}}</a></li>
-        </ul>
     </li>
 </ul>
